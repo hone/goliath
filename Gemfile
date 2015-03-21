6de6cb702f80eb31bbbd5b89f2ccf2bfa6b5d0e7
Terence Lee <hone02@gmail.com>
2015-03-21 18:43:09 -0400

    setup packaging ruby for distributing binary

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ gem "poltergeist"
 gem "concurrent-ruby", require: "concurrent"
 gem "concurrent-ruby-ext"
 
+group :development do
+  gem "rake"
+end
+
 group :test do
   gem "rspec"
 end
---
+++ packaging/wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Figure out where this script is located.
+SELFDIR="`dirname \"$0\"`"
+SELFDIR="`cd \"$SELFDIR\" && pwd`"
+
+# Tell Bundler where the Gemfile and gems are.
+export BUNDLE_GEMFILE="$SELFDIR/lib/vendor/Gemfile"
+unset BUNDLE_IGNORE_CONFIG
+
+export PATH="$SELFDIR/lib/app/bin:$PATH"
+
+# Run the actual app using the bundled Ruby interpreter, with Bundler activated.
+exec "$SELFDIR/lib/ruby/bin/ruby" -rbundler/setup "$SELFDIR/lib/app/bin/goliath" "$@"
